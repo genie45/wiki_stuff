4c512dbaec13e10677b33d7916f403dedee521ce
alex4401 <rylatgl@gmail.com>
2022-09-10 00:24:00 +0200

    Properly load sticky header on translations

--- DefaultTranslationLoadout/Gadget-StickyHeader.js
+++ DefaultTranslationLoadout/Gadget-StickyHeader.js
@@ -1,5 +1,6 @@
 /* ↓ GLOBAL JS FROM ENGLISH WIKI ↓ */
 mw.loader.using( 'site', function () {
-    mw.loader.using( 'en:site',
-        mw.loader.load.bind( mw.loader, '/load.php?lang=en-gb&modules=ext.gadget.StickyHeader&skin=vector' ) );
+    mw.loader.using( 'en:site', function () {
+        mw.loader.load( '/load.php?lang=en&modules=ext.gadget.StickyHeader&skin=vector' );
+    } );
 } );
