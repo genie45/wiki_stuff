22c9d5ae494292f2b66076b0b78c15246b060b43
alex4401 <rylatgl@gmail.com>
2023-08-23 21:05:46 +0200

    refactor!(js/DataFetch): rely on Cloudflare and browser cache
    This change means we don't need to manage request caching on our own, as we can have MediaWiki set required headers.
    
    This may have a slight regression for logged-in users and possibly cause more cache validation requests, but will allow changes to propagate quicker. The DF payload itself is much smaller.

--- js/DataFetch.js
+++ js/DataFetch.js
@@ -1,89 +1,37 @@
-(function() {
-    
-    var CACHE_NAME = 'ARKData';
-    
-    var shouldBypassCache = window.location.search.substr(1).split('&').indexOf('arkdata=no-cache') >= 0;
+var shouldBypassCache = window.location.search.substr( 1 ).split( '&' ).indexOf( 'arkdata=no-cache' ) >= 0;
+var CACHE_DURATION = shouldBypassCache ? 1 : 60 * 60 * 36;
 
-    // Internal implementation
-    function fetchDataPagesInternal(pages, cache, forceRecacheId, expiryTime, skipRecacheId) {
-        function fetchDataPageInternal(pageName) {
-            // Construct a URL of the page.
-            // On translations if prefixed with "en:", this'll slice off the script path.
-            var isRequestingMain = pageName.startsWith('en:');
-            var scriptPath = mw.config.get('wgScriptPath');
-            var urlNoCb = mw.util.getUrl((isRequestingMain ? pageName.slice(3) : pageName), {
+window.arkLoadDataPages = function( pages, forceRecacheId ) {
+    var results = {},
+        scriptPath = mw.config.get( 'wgScriptPath' );
+
+    return Promise.all( pages.map( function ( pageName ) {
+        var isRequestingMain = pageName.startsWith( 'en:' ),
+            query = {
                 action: 'raw',
-                ctype: 'application/json'
-            });
-            // If cache should be bypassed via ?arkdata=no-cache, set the version on request to current
-            // timestamp, so server serves us the latest blob.
-            var url = urlNoCb + '&version=' + ( shouldBypassCache ? Date.now() : forceRecacheId );
-            if (isRequestingMain && mw.config.get('wgContentLanguage') != 'en' && url.startsWith(scriptPath)) {
-                url = url.slice(scriptPath.length);
-            }
-            
-            // If cache should be bypassed (as requested via ?arkdata), null the reference to the cache
-            // manager. This will force taking the short fetch() path.
-            if (shouldBypassCache) {
-                cache = null;
-            }
-
-            var request = new Request(url),
-                cacheRequest = skipRecacheId ? new Request(urlNoCb) : request;
-
-            if (cache != null) {
-                // Cache is available
-                var timeNow = new Date().getTime();
-                return cache.match(cacheRequest).then(function (response) {
-                    // Check if cache entry is recent and valid
-                    if (response && response.ok
-                        && (Date.parse(response.headers.get('Expires')) > timeNow)) {
-                        return response;
-                    }
-
-                    // Fetch the page from API
-                    return fetch(request).then(function (response) {
-                        response.clone().blob().then(function(body) {
-                            cache.put(cacheRequest, new Response(body, { headers: {
-                                'Expires': (new Date(timeNow + expiryTime)).toUTCString(),
-                            }}));
-                        });
-                        return response;
-                    });
-                }).then(function (response) {
-                    return response.json().then(function(data) {
-                        results[pageName] = data;
-                    });
-                });
-            } else {
-                // Cache is unavailable
-                return fetch(request).then(function (response) {
-                    return response.json().then(function(data) {
-                        results[pageName] = data;
-                    });
-                });
-            }
+                ctype: 'application/json',
+                maxage: CACHE_DURATION,
+                smaxage: CACHE_DURATION,
+            };
+        if ( forceRecacheId ) {
+            query.cb = forceRecacheId;
+        }
+        if ( isRequestingMain ) {
+            pageName = pageName.slice( 3 );
         }
 
-        // Create a fetch request for every page, and wait for each to be resolved (or one to fail), then return the results
-        // object.
-        var results = {};
-        return Promise.all(pages.map(function (page) {
-            return fetchDataPageInternal(page);
-        })).then(function () {
-            return Promise.resolve(results);
-        });
-    }
+        var url = mw.util.getUrl( pageName, query );
+        if ( isRequestingMain && mw.config.get( 'wgContentLanguage' ) !== 'en' && url.startsWith( scriptPath ) ) {
+            url = url.slice( scriptPath.length );
+        }
 
-    // Public entrypoint
-    window.arkLoadDataPages = function(pages, forceRecacheId, expiryTime, skipRecacheId) {
-        return caches.open(CACHE_NAME).then(function (cache) {
-            // Cache successfully opened
-            return fetchDataPagesInternal(pages, cache, forceRecacheId, expiryTime, skipRecacheId);
-        }, function() {
-            // Cache not opened. Most likely rejected by browser's privacy settings (e.g. Firefox's incognito mode).
-            return fetchDataPagesInternal(pages, null, forceRecacheId, null, null);
-        });
-    };
+        return fetch( url ).then( function ( response ) {
+            return response.json().then( function ( data ) {
+                results[pageName] = data;
+            } );
+        } );
+    } ) ).then( function () {
+        return Promise.resolve( results );
+    } );
+};
 
-})();
