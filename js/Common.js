33fa3aa9bea4c53ae48e33eb8000872f727563e3
alex4401 <rylatgl@gmail.com>
2023-11-19 17:00:57 +0100

    refactor(js): move legacy element animator out of common.js

--- js/Common.js
+++ js/Common.js
@@ -81,6 +81,9 @@ window.arkConditionalModules = (window.arkConditionalModules||[]).concat( [
         // Experimental cloning calculator
         'en:MediaWiki:CloningCalculator.js' 
     ] },
+    // Legacy element animator
+    { sel: '.animated',
+      pages: [ 'en:MediaWiki:LegacyElementAnimator.js' ] },
     // Legacy [[Template:Nav creatures]] grid filtering
     { sel: '#creature-grid',
       pages: [ 'MediaWiki:CreatureGridFiltering.js' ] },
@@ -475,48 +478,6 @@ $(function() {
             sectionElement.classList.toggle( 'arkitect-is-collapsed' );
         } );
     } );
-    // #endregion
-
-    // #region Element animator - cycles through a set of elements on a timer (modified from minecraft.gamepedia.com)
-    // Add the "animated" class to the frame containing the elements to animate.
-    // Optionally, add the "animated-active" class to the frame to display first.
-    // Optionally, add the "animated-subframe" class to a frame, and the
-    // "animated-active" class to a subframe within, in order to designate a set of
-    // subframes which will only be cycled every time the parent frame is displayed.
-    // Animations with the "animated-paused" class will be skipped each interval.
-    ( function() {
-        var $content = $( '#mw-content-text' );
-        var advanceFrame = function(parentElem, parentSelector) {
-          var curFrame = parentElem.querySelector(parentSelector + ' > .animated-active');
-          $(curFrame).removeClass('animated-active');
-          var $nextFrame = $(curFrame && curFrame.nextElementSibling || parentElem.firstElementChild);
-          return $nextFrame.addClass('animated-active');
-        };
-
-        setInterval(function() {
-            if (document.hidden) {
-                return;
-            }
-            $content.find('.animated').each(function() {
-                if ($(this).hasClass('animated-paused')) {
-                    return;
-                }
-
-                var $nextFrame = advanceFrame(this, '.animated');
-                if ($nextFrame.hasClass('animated-subframe')) {
-                    advanceFrame($nextFrame[0], '.animated-subframe');
-                }
-            });
-        }, 5000);
-    } )();
-    /**
-     * Pause animations on mouseover of a designated container (.animated-container)
-     *
-     * This is so people have a chance to look at the image and click on pages they want to view.
-     */
-    $('#mw-content-text').on('mouseenter mouseleave', '.animated-container', function (e) {
-        $(this).find('.animated').toggleClass('animated-paused', e.type === 'mouseenter');
-    });
     // #endregion
 
     // #region Translation advertising banners
