bf33eeff046cbeb90dbdae6717d0c332af3dc2ce
alex4401 <rylatgl@gmail.com>
2024-01-21 19:03:00 +0100

    feat(js): command filtering prototype

---
+++ js/CommandFilters.js
@@ -0,0 +1,79 @@
+function buildCommandIndex( contentElement ) {
+    var results = [];
+    contentElement.querySelectorAll( 'div.console-command' ).forEach( function ( element ) {
+        var tags = ( element.getAttribute( 'data-tags' ) || '' )
+            .split( ';' )
+            .filter( function ( element ) {
+                return element.length > 0;
+            } );
+
+        if ( element.getAttribute( 'data-asa' ) === '1' ) {
+            tags.push( 'asa' );
+        }
+        if ( element.getAttribute( 'data-ase' ) === '1' ) {
+            tags.push( 'ase' );
+        }
+
+        results.push( {
+            element: element,
+            name: element.getAttribute( 'data-name' ),
+            isCheat: element.getAttribute( 'data-cheat' ) === '1' ? true : false,
+            tags: tags,
+        } );
+    } );
+    return results;
+}
+
+
+function gatherTags( setupElement ) {
+    var results = {};
+    var namePrefix = 'data-tag-';
+    Object.getOwnPropertyNames( setupElement.attributes )
+        .filter( function ( name ) {
+            return name.startsWith( namePrefix );
+        } )
+        .forEach( function ( name ) {
+            results[ name.slice( namePrefix.length ) ] = setupElement.attributes[ name ].value;
+        } );
+    return results;
+}
+
+
+function constructSearchBar( commandIndex ) {
+    var searchInput;
+
+    function _reevaluate() {
+        var query = searchInput.value.trim().toLowerCase();
+        commandIndex.forEach( function ( command ) {
+            var isVisible = command.name.toLowerCase().includes( query );
+            command.element.style.display = isVisible ? '' : 'none';
+        } );
+    }
+
+    var container = document.createElement( 'div' );
+    container.className = 'console-filters__search-box';
+    
+    searchInput = document.createElement( 'input' );
+    searchInput.type = 'text';
+    searchInput.placeholder = 'Start typing to search through commands...';
+    
+    searchInput.addEventListener( 'change', _reevaluate );
+    searchInput.addEventListener( 'keydown', _reevaluate );
+    
+    container.appendChild( searchInput );
+    return container;
+}
+
+
+function main() {
+	var container = document.querySelector( '#console-filters' ),
+        tagRegistry = gatherTags( container )
+    	commandIndex = buildCommandIndex( container.parentElement );
+    
+    container.appendChild( constructSearchBar( commandIndex ) );
+
+	container.setAttribute( 'data-loaded', true );
+}
+
+
+main();
